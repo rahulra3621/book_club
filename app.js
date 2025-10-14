4083bd61e2681e914abe5647b5ef67f86037c607
Rahul Rajput <rahulrajput3621@gmail.com>
2025-10-14 23:56:22 +0530

    User Autharisation using JWT and Password encryption

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookie_parser = require('cookie-parser')
 const app = express();
 const mongoose = require('mongoose');
-const { type } = require('os');
 const path = require('path');
-const uri = 'mongodb://localhost:27017/bookClubApp';
+const uri = 'mongodb+srv://rahulrajput3621_db_user:Rahul%40mongo123@todo.fntphjl.mongodb.net/bookClub';
+const SECRET_KEY = "JWT_SECRET_KEY";
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cookie_parser());
+
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -42,8 +49,7 @@ const bookSchema = new mongoose.Schema({
         type: mongoose.Schema.ObjectId,
         default: null,
     }
-})
-
+});
 const bookModel = mongoose.model('book', bookSchema);
 
 const userSchema = new mongoose.Schema({
@@ -61,7 +67,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    userwallet:{
+    userwallet: {
         type: Number,
         default: 0,
     },
@@ -70,8 +76,32 @@ const userSchema = new mongoose.Schema({
         default: null,
     },
 });
-
 const userModel = mongoose.model('user', userSchema);
+
+// ---------------------------------- AuthMiddleware Functionality -------------------------------
+
+async function authMiddleware(req, res, next) {
+    try {
+        let token;
+        if (!token && req.cookies.token && req.cookies) {
+            token = req.cookies.token;
+        }
+        if (!token) {
+            return res.send('Access Denied. No Token Provided')
+        }
+        const verifiedUser = await jwt.verify(token, SECRET_KEY);
+        if (!verifiedUser) {
+            res.json({
+                response: "Unauthorized Access!!!",
+            })
+        }
+        req.id = verifiedUser.id;
+        next();
+    } catch (e) {
+        res.cookie('err', `Token Expired! Login Again`);
+        return res.redirect('/login')
+    }
+}
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -80,32 +110,42 @@ app.get('/', (req, res) => {
 //------------------------------------- Log In --------------------------------------------------
 
 app.route('/login')
-    .get( (req, res) => {
-        res.render('login', { err: null });
+    .get((req, res) => {
+        let err = req.cookies.err;
+        res.render('login', { err });
     })
-    .post( async (req, res) => {
+    .post(async (req, res) => {
         const { email, password } = req.body;
         const user = await userModel.findOne({
             usermail: email,
-            userpasswd: password,
         });
-        if (user) {
-            res.redirect(`/home/${user._id}`);
+        if (!user) {
+            res.render('login', { err: 'User Not Found!' })
         }
         else {
-            res.render('login', { err: 'Invalid Email or Password !!!' })
+            const validPassword = await bcrypt.compare(password, user.userpasswd);
+            if (!validPassword) {
+                res.render('login', { err: 'Invalid Credentials !!!' })
+            }
+            else {
+                const token = await jwt.sign({ id: user._id }, SECRET_KEY);
+                res.cookie('token', token, { httpOnly: true })
+                res.redirect(`/home`);
+            }
         }
+
     });
 
 // --------------------------------- Register ------------------------------------------------
 
 app.route('/register')
-    .get( (req, res) => {
+    .get((req, res) => {
         res.render('register', { err: null });
     })
-    .post( async (req, res) => {
+    .post(async (req, res) => {
         const { username, password, email } = req.body;
         if (username && password && email) {
+            const hashedPassword = await bcrypt.hash(password, 5);
             const check = await userModel.findOne({
                 usermail: email,
             })
@@ -115,7 +155,7 @@ app.route('/register')
             else {
                 const user = await userModel.create({
                     username: username,
-                    userpasswd: password,
+                    userpasswd: hashedPassword,
                     usermail: email,
                 })
                 console.log(user);
@@ -130,108 +170,125 @@ app.route('/register')
 // ---------------------------------- Reset password ---------------------------------------
 
 app.route('/reset')
-    .get( (req, res) => {
+    .get((req, res) => {
         res.render('reset', { err: null });
     })
-    .post( async (req, res) => {
+    .post(async (req, res) => {
         const { username, password, email } = req.body;
         const user = await userModel.findOne({
             username: username,
             usermail: email,
         })
         if (user) {
+            const hashedPassword = await bcrypt.hash(password, 5);
             await userModel.findOneAndUpdate(user, {
-                userpasswd: password,
+                userpasswd: hashedPassword,
             })
-            res.render('reset', { err: 'Password Updated Successfully, Login Now!' })
+            res.render('reset', { err: 'Password reset successfully!!' })
         }
         else {
             res.render('reset', { err: 'Cannot find any user with this Name and Email' });
         }
     });
 
+
+
+// -------------------------- Auth Middleware for JWT Authentication ------------------------------------
+
+app.use(authMiddleware);
+
+
 // ---------------------------------- Log Out ---------------------------------------------------
 
 app.route('/logout')
-    .get((req,res)=>{res.render('index')
+    .get((req, res) => {
+        res.cookie('token', null)
+        res.render('index')
     });
+
 
 // ----------------------------------- Homepage -----------------------------------------------
 
-app.get('/home/:id', async (req, res) => {
+app.get('/home', async (req, res) => {
     const books = await bookModel.find();
-    const user = await userModel.findById(req.params.id)
-    res.render('home', {user, books, err:null})
+    const user = await userModel.findById(req.id)
+    res.render('home', { user, books, err: null })
 });
 
 // ------------------------------------- Profile ---------------------------------------------------
 
-app.get('/profile/:id', async (req,res)=>{
-    const user = await userModel.findById(req.params.id);
-    res.render('profile', {user})
+app.get('/profile', async (req, res) => {
+    const user = await userModel.findById(req.id);
+    res.render('profile', { user })
 });
 
 // ----------------------------------------- Book Rent ---------------------------------------
 
-app.get('/home/rent/:userId/:bookId', async (req, res) => {
+app.get('/home/rent/:bookId', async (req, res) => {
     const book = await bookModel.findById(req.params.bookId);
-    const user = await userModel.findById(req.params.userId);
+    const user = await userModel.findById(req.id);
     if (book.isIssued) {
         const books = await bookModel.find();
-        res.render('home', {user, books, id: book._id, err: 'Book is Already Issued to a User!' })
+        res.render('home', { user, books, id: book._id, err: 'Book is Already Issued to a User!' })
     }
     else {
-        res.render('checkout', {user,book});
+        res.render('checkout', { user, book });
     }
 })
 
 // ----------------------------------------------- Checkout ------------------------------------------------------
 
-app.get('/checkout/:userId/:bookId', async (req,res)=>{
-    const user = await userModel.findById(req.params.userId);
-    const book = await bookModel.findByIdAndUpdate(req.params.bookId, {
-        isIssued: true,
-        isIssuedTo: user,
-    });
-    const newAmount = (user.userwallet)-(book.bookPrice);
-    await userModel.findOneAndUpdate(user, {
-        userwallet:newAmount,
-    })
-    res.render('rentSuccess', {user,book});
+app.get('/checkout/:bookId', async (req, res) => {
+    const user = await userModel.findById(req.id);
+    const book = await bookModel.findById(req.params.bookId);
+    if (user.userwallet >= book.bookPrice) {
+        await bookModel.findOneAndUpdate(book, {
+            isIssued: true,
+            isIssuedTo: user,
+        });
+        const newAmount = (user.userwallet) - (book.bookPrice);
+        await userModel.findOneAndUpdate(user, {
+            userwallet: newAmount,
+        })
+        res.render('rentSuccess', { user, book });
+    }
+    else {
+        res.render('recharge', { user, err: "Insufficient Amount! Please Recharge First." })
+    }
 });
 
 // ------------------------------------- My Books -----------------------------
 
-app.route('/mybooks/:id')
-    .get( async (req,res)=>{
-        const user = await userModel.findById(req.params.id);
-        const books  =await bookModel.find();
-        res.render('mybooks', {user, books});
+app.route('/mybooks')
+    .get(async (req, res) => {
+        const user = await userModel.findById(req.id);
+        const books = await bookModel.find();
+        res.render('mybooks', { user, books });
     })
 
 // ------------------------------------ Return Book -----------------------------------
 
-app.route('/mybooks/return/:userId/:bookId')
-    .get( async(req,res)=>{
-        const user = await userModel.findById(req.params.userId);
+app.route('/mybooks/return/:bookId')
+    .get(async (req, res) => {
+        const user = await userModel.findById(req.id);
         const book = await bookModel.findById(req.params.bookId);
-        // const newAmount = (user.userwallet)+(book.bookPrice);
+        const newAmount = (user.userwallet) + ((book.bookPrice) - (book.bookPrice) / 10);
         await bookModel.findOneAndUpdate(book, {
             isIssued: false,
-            isIssuedTo:null,
+            isIssuedTo: null,
         });
-        // await userModel.findOneAndUpdate(user, {
-        //     userwallet:newAmount,
-        // });
-        res.render('returnSuccess', {user,book})
+        await userModel.findOneAndUpdate(user, {
+            userwallet: newAmount,
+        });
+        res.render('returnSuccess', { user, book })
     })
 
 
 // ---------------------------------- Book Search Functionality ---------------------------------
 
-app.get('/search/:id', async (req,res)=>{
-    const user = await userModel.findById(req.params.id);
-    var {q} = (req.query);
+app.get('/search', async (req, res) => {
+    const user = await userModel.findById(req.id);
+    var { q } = (req.query);
     q = q.toLowerCase();
 
     console.log(q);
@@ -240,34 +297,33 @@ app.get('/search/:id', async (req,res)=>{
 
     if (q) {
         allbooks.forEach(book => {
-            if (book.bookName.toLowerCase().includes(q) || book.bookAuthor.toLowerCase().includes(q) || book.bookPublisher.toLowerCase().includes(q)){
+            if (book.bookName.toLowerCase().includes(q) || book.bookAuthor.toLowerCase().includes(q) || book.bookPublisher.toLowerCase().includes(q)) {
                 books.push(book);
             }
         });
-        res.render('home', {user,books, err:null});
+        res.render('home', { user, books, err: null });
     }
 
 });
 
 
 // -------------------------------------------- Recharge ---------------------------------------------------------------
+app.route('/recharge')
+    .get(async (req, res) => {
+        const user = await userModel.findById(req.id);
+        res.render('recharge', { user, err: null });
+    })
+    .post(async (req, res) => {
+        const { credit } = req.body;
+        const user = await userModel.findByIdAndUpdate(req.id);
+        const newAmount = (user.userwallet) + Number(credit);
+        await userModel.findOneAndUpdate(user, {
+            userwallet: newAmount,
+        });
+        res.redirect('/profile');
 
-app.get('/recharge/:id', async (req,res)=>{
-    const user = await userModel.findById(req.params.id);
-    res.render('recharge', {user});
-});
 
-app.post('/recharge/:id', async (req,res)=>{
-    const {credit} = req.body;
-    const user = await userModel.findByIdAndUpdate(req.params.id);
-    const newAmount = (user.userwallet)+Number(credit);
-    await userModel.findOneAndUpdate(user, {
-        userwallet:newAmount,
     });
-    res.redirect('/profile/'+user._id);
-
-
-});
 
 
 // ----------------------------------- App Listener ----------------------------------------------------------------------
